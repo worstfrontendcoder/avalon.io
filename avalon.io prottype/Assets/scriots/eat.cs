2f34d8a0bb330fdee40e3d5aabc56478f00df1ad
worstfrontendcoder <omarsumon1030@gmail.com>
2021-03-02 21:59:46 -0600

    unity thing

---
+++ avalon.io prottype/Assets/scriots/eat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eat : MonoBehaviour
+{
+    public float increase;
+    public float decrease;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "food")
+        {
+            transform.localScale += new Vector3(increase, increase, increase);
+            Destroy(other.gameObject);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            transform.localScale += new Vector3(decrease, decrease, decrease);
+        };
+    }
+}
---
+++ avalon.io prottype/Assets/scriots/food.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+
+
+    public GameObject originalFood;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Genreat(3);
+    }
+
+    void Genreat(int foodsNum)
+    {
+        for (int i = 0; i < foodsNum; i++)
+        {
+            GameObject foodClone = Instantiate(originalFood, new Vector3(i, originalFood.transform.position.y, i), originalFood.transform.rotation );
+        };
+    }
+}
